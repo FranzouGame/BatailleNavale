17a1e1e8c2c35d736c833de055a43ada057a8d18
FranzouGame <franz04@outlook.fr>
2023-11-16 00:07:26 +0100

    Tir/SaisiVerif

--- SAE/main.cpp
+++ SAE/main.cpp
@@ -5,19 +5,28 @@ But : Jouer à la bataille navale
 
 #include "game-tools.h"
 #include <iostream>
+#include <tuple>
+#include <string>
 using namespace std;
 
 const unsigned short int NB_CASES = 9; // Taille du tableau horizontalement et verticalement
 char plateauJeu[NB_CASES][NB_CASES];   // Tableau contenant le plateau de jeu
 
+int tourJoueur = 0;
+tuple<int, int> bateauJoueur[2][4];
+
 // Déclaration des sous-programmes
-/* void generePlateau(char tab[][], unsigned short int lgTab); */
-void afficherTableau();
-// But : Générer un plateau de jeu à 2 dimensions tab de taille lgTab
+void afficherTableau();                 // A commenter
+void genererBateau();                   // A faire
+void nouveauTour();                     // A completer
+void tirJoueur(int ligne, int colonne); // A complter
+void verifBateauToucher();              // A faire
 
 int main(void)
 {
-    afficherTableau();
+    genererBateau();
+    nouveauTour();
+    cin >> tourJoueur;
 }
 
 void afficherTableau()
@@ -26,7 +35,7 @@ void afficherTableau()
     {
         for (int j = 0; j <= NB_CASES + 1; j++) // COLLONES
         {
-            if (i == 0 || i == NB_CASES + 1)
+            if (i == 0 || i == NB_CASES + 1) // Afficher lettres haut et bas
             {
                 if (j == 0 || j == NB_CASES + 1)
                 {
@@ -37,7 +46,7 @@ void afficherTableau()
                     cout << char('A' + (j - 1)) << " ";
                 }
             }
-            else
+            else // Afficher les chiffres gauches et droite
             {
                 if (j == 0 || j == NB_CASES + 1)
                 {
@@ -51,4 +60,54 @@ void afficherTableau()
         }
         cout << endl;
     }
+}
+
+void genererBateau()
+{
+}
+
+void nouveauTour()
+{
+    string action;
+    cout << "Votre tir : ";
+    cin >> action;
+    int ligne = (int)action[1] - 48;
+    int colonne = int(action[0]) - 64;
+
+    if (action.length() == 2 && colonne > 0 && colonne < 10 && ligne > 0 && ligne < 10)
+    {
+        tirJoueur(ligne, colonne);
+    }
+    else
+    {
+        cout << "tir par valide" << endl;
+        nouveauTour();
+        return;
+        // A faire les vérification derreur
+    }
+
+    afficherTableau();
+}
+
+void tirJoueur(int ligne, int colonne)
+{
+    int bateauToucher = 0;
+    // 0 = non, 1=joueur 1, 2=joueur2
+
+    if (bateauToucher == 0)
+    {
+        plateauJeu[ligne][colonne] = '.';
+    }
+    else if (bateauToucher == 1)
+    {
+        plateauJeu[ligne][colonne] = 'O';
+    }
+    else if (bateauToucher == 1)
+    {
+        plateauJeu[ligne][colonne] = 'X';
+    }
+}
+
+void verifBateauToucher()
+{
 }
