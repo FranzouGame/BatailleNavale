ac4fb0c0bbafa828b06642153c49605bf25c1bdf
Lifzerr <maxime.bourciez@gmail.com>
2023-11-20 14:49:21 +0100

    More comments

--- SAE/main.cpp
+++ SAE/main.cpp
@@ -90,9 +90,16 @@ void genererBateau()
 void nouveauTour()
 {
     // Variables locales
-    string action;
-    int ligne;
-    int colonne;
+    string action;                                          // action du joueur
+    int ligne;                                              // ligne du tir du joueur
+    int colonne;                                            // colonne du tir du joueur
+    const unsigned short int INDICE_LETTRE_MIN = 97;        // Indice minimal de lettre dans la table ASCII
+    const unsigned short int INDICE_LETTRE_MAX = 105;       // Indice maximal de lettre dans la table ASCII
+    const unsigned short int CONVERSION_LETTRE_MINUSC = 64; // Valeur pour convertir un caractère lettré minuscule en nombre entier
+    const unsigned short int CONVERSION_LETTRE_MAJUSC = 96; // Valeur pour convertir un caractère lettré majuscule en nombre entier
+    const unsigned short int CONVERSION_ENTIER = 48;        // Valeur pour convertir un nombre fourni dans une chaine de caractères en entier
+    const unsigned short int INDICE_VERIF_MIN = 0;          // Indice minimal pour la vérification de la saisie
+    const unsigned short int INDICE_VERIF_MAX = 10;         // Indice maximal pour la vérification de la saisie
 
     // Traitements
     // Saisie de la cible du tir par l'utilisisateur
@@ -100,36 +107,44 @@ void nouveauTour()
     cin >> action;
 
     // Conversion du retour utilisateur pour pouvoir placer le tir sur la grille
-    if ( int(action[0]) >= 97 && int(action[0]) < 105 )
+    if (int(action[0]) >= INDICE_LETTRE_MIN && int(action[0]) < INDICE_LETTRE_MAX)
     {
-        colonne = int(action[0]) - 96;
+        colonne = int(action[0]) - CONVERSION_LETTRE_MAJUSC;
     }
     else
     {
-        colonne = int(action[0]) - 64;
+        colonne = int(action[0]) - CONVERSION_LETTRE_MINUSC;
     }
-    
-    ligne = (int)action[1] - 48;
+
+    ligne = (int)action[1] - CONVERSION_ENTIER;
 
     // Vérification de la validité du tir
-    if (action.length() == 2 && colonne > 0 && colonne < 10 && ligne > 0 && ligne < 10)
+    if (action.length() == 2 && colonne > INDICE_VERIF_MIN && colonne < INDICE_VERIF_MAX && ligne > INDICE_VERIF_MIN && ligne < INDICE_VERIF_MAX)
     {
         tirJoueur(ligne, colonne);
     }
+
+    // Gestion des erreurs de saisie
     else
     {
-        if ((ligne < 10 || ligne > 1) && (colonne > 0 && colonne < 10))
+        // Vérification d'une éventuelle erreur en Y
+        if ((ligne < INDICE_VERIF_MAX || ligne > INDICE_VERIF_MIN + 1) && (colonne > INDICE_VERIF_MIN && colonne < INDICE_VERIF_MAX))
         {
+            // Affichage du message d'erreur
             cout << "### Erreur en Y ###" << endl;
+            // Nouvelle saisie utilisateur
             nouveauTour();
         }
-        else if (colonne < 0 || colonne > 9)
+        // Vérification d'une éventuelle erreur en X
+        else if (colonne < INDICE_VERIF_MIN || colonne >= INDICE_VERIF_MAX)
         {
+            // Afficher le message d'erreur
             cout << "### Erreur en X ###" << endl;
+            // Nouvelle saisie utilisateur
             nouveauTour();
         }
     }
-
+    // Affichage du tableau après tir
     afficherTableau();
 }
 
